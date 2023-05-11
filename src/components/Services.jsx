47e38d962b5e7021bf59b6be49467843b6a5e8a1
Felipe Justus Matos <70666131+FelipeJustusMatos@users.noreply.github.com>
2023-05-11 15:02:44 -0300

    teste2

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -17,98 +17,98 @@ import { airport, beach, park } from '../assets/images/index.js';
 export default function Services() {
   return (
     <>
-      <Center pt={5}>
-        <Card>
-          <Center>
-            <Box borderBottom={'1px solid black'}>
-              <Text textAlign={'center'} as="b">
-                CATEGORIES
+      {/* <Center pt={5}> */}
+      <Card>
+        <Center>
+          <Box borderBottom={'1px solid black'}>
+            <Text textAlign={'center'} as="b">
+              CATEGORIES
+            </Text>
+          </Box>
+        </Center>
+
+        <SimpleGrid
+          marginTop={5}
+          spacing={4}
+          columns={{ sm: 1, md: 3 }}
+          w={'800px'}
+          flexDirection={'row'}
+        >
+          {/* AIRPORT */}
+          <Card textAlign={'center'} shadow={'xl'}>
+            <CardHeader>
+              <Center>
+                <Box>
+                  <Image src={airport} alt="airport" borderRadius="md" />
+                </Box>
+              </Center>
+              <Heading size="md">AIRPORT</Heading>
+            </CardHeader>
+            <CardBody textAlign={'center'}>
+              <Text>
+                High-quality transport service with experienced multilingual
+                drivers and versatile fleet. Seamless airport transfers to
+                renowned Algarve towns like Albufeira, Lagos, Vilamoura, and
+                more.
               </Text>
-            </Box>
-          </Center>
+            </CardBody>
+            <Center>
+              <CardFooter>
+                <Button bg="#ECB939">View here</Button>
+              </CardFooter>
+            </Center>
+          </Card>
 
-          <SimpleGrid
-            marginTop={5}
-            spacing={4}
-            columns={{ sm: 1, md: 3 }}
-            w={'800px'}
-            flexDirection={'row'}
-          >
-            {/* AIRPORT */}
-            <Card textAlign={'center'} shadow={'xl'}>
-              <CardHeader>
-                <Center>
-                  <Box>
-                    <Image src={airport} alt="airport" borderRadius="md" />
-                  </Box>
-                </Center>
-                <Heading size="md">AIRPORT</Heading>
-              </CardHeader>
-              <CardBody textAlign={'center'}>
-                <Text>
-                  High-quality transport service with experienced multilingual
-                  drivers and versatile fleet. Seamless airport transfers to
-                  renowned Algarve towns like Albufeira, Lagos, Vilamoura, and
-                  more.
-                </Text>
-              </CardBody>
+          {/* PARK */}
+          <Card textAlign={'center'} shadow={'xl'}>
+            <CardHeader>
               <Center>
-                <CardFooter>
-                  <Button bg="#ECB939">View here</Button>
-                </CardFooter>
+                <Box>
+                  <Image src={park} alt="airport" borderRadius="md" />
+                </Box>
               </Center>
-            </Card>
+              <Heading size="md">THEME PARK</Heading>
+            </CardHeader>
+            <CardBody textAlign={'center'}>
+              <Text>
+                Convenient and comfortable transfers between Algarve hotels and
+                top amusement parks. Flexible scheduling and online reservations
+                for a hassle-free and enjoyable experience.
+              </Text>
+            </CardBody>
+            <Center>
+              <CardFooter>
+                <Button bg="#ECB939">View here</Button>
+              </CardFooter>
+            </Center>
+          </Card>
 
-            {/* PARK */}
-            <Card textAlign={'center'} shadow={'xl'}>
-              <CardHeader>
-                <Center>
-                  <Box>
-                    <Image src={park} alt="airport" borderRadius="md" />
-                  </Box>
-                </Center>
-                <Heading size="md">THEME PARK</Heading>
-              </CardHeader>
-              <CardBody textAlign={'center'}>
-                <Text>
-                  Convenient and comfortable transfers between Algarve hotels
-                  and top amusement parks. Flexible scheduling and online
-                  reservations for a hassle-free and enjoyable experience.
-                </Text>
-              </CardBody>
+          {/* BEACH */}
+          <Card textAlign={'center'} shadow={'xl'}>
+            <CardHeader>
               <Center>
-                <CardFooter>
-                  <Button bg="#ECB939">View here</Button>
-                </CardFooter>
+                <Box>
+                  <Image src={beach} alt="airport" borderRadius="md" />
+                </Box>
               </Center>
-            </Card>
-
-            {/* BEACH */}
-            <Card textAlign={'center'} shadow={'xl'}>
-              <CardHeader>
-                <Center>
-                  <Box>
-                    <Image src={beach} alt="airport" borderRadius="md" />
-                  </Box>
-                </Center>
-                <Heading size="md">BEACH</Heading>
-              </CardHeader>
-              <CardBody textAlign={'center'}>
-                <Text>
-                  Premier transportation services for exploring Algarve's
-                  beautiful beaches. Trust our experienced drivers for a
-                  memorable and convenient beach getaway.
-                </Text>
-              </CardBody>
-              <Center>
-                <CardFooter>
-                  <Button bg="#ECB939">View here</Button>
-                </CardFooter>
-              </Center>
-            </Card>
-          </SimpleGrid>
-        </Card>
-      </Center>
+              <Heading size="md">BEACH</Heading>
+            </CardHeader>
+            <CardBody textAlign={'center'}>
+              <Text>
+                Premier transportation services for exploring Algarve's
+                beautiful beaches. Trust our experienced drivers for a memorable
+                and convenient beach getaway.
+              </Text>
+            </CardBody>
+            <Center>
+              <CardFooter>
+                <Button bg="#ECB939">View here</Button>
+              </CardFooter>
+            </Center>
+          </Card>
+        </SimpleGrid>
+      </Card>
+      {/* </Center> */}
     </>
   );
 }
