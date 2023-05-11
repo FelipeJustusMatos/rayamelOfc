278401a4f34c01209d5562e36a862577bd05a9a5
Felipe Justus Matos <70666131+FelipeJustusMatos@users.noreply.github.com>
2023-05-11 10:42:33 -0300

    UpdateServices

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardBody,
@@ -10,6 +11,7 @@ import {
   Text,
 } from '@chakra-ui/react';
 import React from 'react';
+import { Image } from '@chakra-ui/react';
 
 export default function Services() {
   return (
@@ -22,9 +24,30 @@ export default function Services() {
           w={'800px'}
           flexDirection={'row'}
         >
+          <Card textAlign={'center'}>
+            <CardHeader>
+              <Center>
+                <Image
+                  borderRadius="md"
+                  boxSize="150px"
+                  src="https://bit.ly/dan-abramov"
+                  alt="Dan Abramov"
+                />
+              </Center>
+              <Heading size="md">AIRPORT</Heading>
+            </CardHeader>
+            <CardBody textAlign={'center'}>
+              <Text>
+                View a summary of all your customers over the last month.
+              </Text>
+            </CardBody>
+            <CardFooter>
+              <Button>View here</Button>
+            </CardFooter>
+          </Card>
           <Card>
             <CardHeader>
-              <Heading size="md"> Customer dashboard</Heading>
+              <Heading size="md"> THEME PARKS</Heading>
             </CardHeader>
             <CardBody>
               <Text>
@@ -37,20 +60,7 @@ export default function Services() {
           </Card>
           <Card>
             <CardHeader>
-              <Heading size="md"> Customer dashboard</Heading>
-            </CardHeader>
-            <CardBody>
-              <Text>
-                View a summary of all your customers over the last month.
-              </Text>
-            </CardBody>
-            <CardFooter>
-              <Button>View here</Button>
-            </CardFooter>
-          </Card>
-          <Card>
-            <CardHeader>
-              <Heading size="md"> Customer dashboard</Heading>
+              <Heading size="md"> BEACHES</Heading>
             </CardHeader>
             <CardBody>
               <Text>
