09197734fd13d4ad962c935740e4bf64975865a4
Felipe Justus Matos <70666131+FelipeJustusMatos@users.noreply.github.com>
2023-05-11 16:20:57 -0300

    UpdateFirstPage

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,3 +2,4 @@ export { default as logo } from './logo.png';
 export { default as airport } from './airport.jpg';
 export { default as beach } from './beach.jpeg';
 export { default as park } from './park.webp';
+export { default as theme } from './theme.jpg';
--- src/components/Firstscreen.jsx
+++ src/components/Firstscreen.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Box,
   Button,
@@ -5,41 +6,42 @@ import {
   Card,
   CardBody,
   CardFooter,
+  Image,
   Heading,
   Text,
   VStack,
 } from '@chakra-ui/react';
-import React from 'react';
+import { theme } from '../assets/images/index.js';
 
 export default function Firstscreen() {
   return (
     <Card justifyContent={'center'}>
       <Box
-        backgroundImage="linear-gradient(to right, rgba(0, 0, 0, 0.9), transparent), url(https://www.aracaju.se.leg.br/institucional/noticias/no-dia-dos-motoristas-binho-ressalta-a-importancia-do-motorista-de-aplicativo-em-nossa-sociedade/image_large)"
+        backgroundImage={`linear-gradient(to right, rgba(0, 0, 0, 0.9), transparent), url(${theme})`}
         backgroundPosition="center"
         backgroundRepeat="no-repeat"
         backgroundSize="cover"
-        w={'100%'}
+        w="100%"
         minHeight={['55vh', '80vh']}
         display="flex"
         alignItems="center"
         justifyContent="flex-start"
       >
-        <VStack fontSize={'lg'} pt={20} spacing={3} textAlign="left">
+        <VStack fontSize="lg" pt={20} spacing={3} textAlign="left">
           <CardBody>
-            <Text color={'white'}>YOUR BEST TRAVEL</Text>
-            <Heading color={'white'}>An Unforgettable Ride</Heading>
-            <Text color={'white'}>
+            <Text color="white">YOUR BEST TRAVEL</Text>
+            <Heading color="white">An Unforgettable Ride</Heading>
+            <Text color="white">
               Let us take care of your transport. Just enjoy the view!
             </Text>
           </CardBody>
           <CardFooter flexDirection="row">
             <Box>
               <ButtonGroup>
-                <Button bg="#ECB939" borderRadius={'md'}>
+                <Button bg="#ECB939" borderRadius="md">
                   Click here
                 </Button>
-                <Button colorScheme="gray" borderRadius={'md'}>
+                <Button colorScheme="gray" borderRadius="md">
                   Click here
                 </Button>
               </ButtonGroup>
