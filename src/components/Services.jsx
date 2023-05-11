297a3c3507253e776a0ab359984072dcbe3f6ce6
Felipe Justus Matos <70666131+FelipeJustusMatos@users.noreply.github.com>
2023-05-11 15:00:39 -0300

    teste

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -17,87 +17,97 @@ import { airport, beach, park } from '../assets/images/index.js';
 export default function Services() {
   return (
     <>
-      <Center>
-        <SimpleGrid
-          marginTop={5}
-          spacing={4}
-          columns={{ sm: 1, md: 3 }}
-          w={'800px'}
-          flexDirection={'row'}
-        >
-          {/* AIRPORT */}
-          <Card textAlign={'center'} shadow={'xl'}>
-            <CardHeader>
-              <Center>
-                <Box>
-                  <Image src={airport} alt="airport" borderRadius="md" />
-                </Box>
-              </Center>
-              <Heading size="md">AIRPORT</Heading>
-            </CardHeader>
-            <CardBody textAlign={'center'}>
-              <Text>
-                High-quality transport service with experienced multilingual
-                drivers and versatile fleet. Seamless airport transfers to
-                renowned Algarve towns like Albufeira, Lagos, Vilamoura, and
-                more.
+      <Center pt={5}>
+        <Card>
+          <Center>
+            <Box borderBottom={'1px solid black'}>
+              <Text textAlign={'center'} as="b">
+                CATEGORIES
               </Text>
-            </CardBody>
-            <Center>
-              <CardFooter>
-                <Button bg="#ECB939">View here</Button>
-              </CardFooter>
-            </Center>
-          </Card>
+            </Box>
+          </Center>
 
-          {/* PARK */}
-          <Card textAlign={'center'} shadow={'xl'}>
-            <CardHeader>
+          <SimpleGrid
+            marginTop={5}
+            spacing={4}
+            columns={{ sm: 1, md: 3 }}
+            w={'800px'}
+            flexDirection={'row'}
+          >
+            {/* AIRPORT */}
+            <Card textAlign={'center'} shadow={'xl'}>
+              <CardHeader>
+                <Center>
+                  <Box>
+                    <Image src={airport} alt="airport" borderRadius="md" />
+                  </Box>
+                </Center>
+                <Heading size="md">AIRPORT</Heading>
+              </CardHeader>
+              <CardBody textAlign={'center'}>
+                <Text>
+                  High-quality transport service with experienced multilingual
+                  drivers and versatile fleet. Seamless airport transfers to
+                  renowned Algarve towns like Albufeira, Lagos, Vilamoura, and
+                  more.
+                </Text>
+              </CardBody>
               <Center>
-                <Box>
-                  <Image src={park} alt="airport" borderRadius="md" />
-                </Box>
+                <CardFooter>
+                  <Button bg="#ECB939">View here</Button>
+                </CardFooter>
               </Center>
-              <Heading size="md">THEME PARK</Heading>
-            </CardHeader>
-            <CardBody textAlign={'center'}>
-              <Text>
-                Convenient and comfortable transfers between Algarve hotels and
-                top amusement parks. Flexible scheduling and online reservations
-                for a hassle-free and enjoyable experience.
-              </Text>
-            </CardBody>
-            <Center>
-              <CardFooter>
-                <Button bg="#ECB939">View here</Button>
-              </CardFooter>
-            </Center>
-          </Card>
+            </Card>
 
-          {/* BEACH */}
-          <Card textAlign={'center'} shadow={'xl'}>
-            <CardHeader>
+            {/* PARK */}
+            <Card textAlign={'center'} shadow={'xl'}>
+              <CardHeader>
+                <Center>
+                  <Box>
+                    <Image src={park} alt="airport" borderRadius="md" />
+                  </Box>
+                </Center>
+                <Heading size="md">THEME PARK</Heading>
+              </CardHeader>
+              <CardBody textAlign={'center'}>
+                <Text>
+                  Convenient and comfortable transfers between Algarve hotels
+                  and top amusement parks. Flexible scheduling and online
+                  reservations for a hassle-free and enjoyable experience.
+                </Text>
+              </CardBody>
               <Center>
-                <Box>
-                  <Image src={beach} alt="airport" borderRadius="md" />
-                </Box>
+                <CardFooter>
+                  <Button bg="#ECB939">View here</Button>
+                </CardFooter>
               </Center>
-              <Heading size="md">BEACH</Heading>
-            </CardHeader>
-            <CardBody textAlign={'center'}>
-              <Text>
-                Premier transportation services for exploring Algarve's
-                beautiful beaches. Trust our experienced drivers for a memorable
-                and convenient beach getaway.
-              </Text>
-            </CardBody>
-            <Center>
-              <CardFooter>
-                <Button bg="#ECB939">View here</Button>
-              </CardFooter>
-            </Center>
-          </Card>
-        </SimpleGrid>
+            </Card>
+
+            {/* BEACH */}
+            <Card textAlign={'center'} shadow={'xl'}>
+              <CardHeader>
+                <Center>
+                  <Box>
+                    <Image src={beach} alt="airport" borderRadius="md" />
+                  </Box>
+                </Center>
+                <Heading size="md">BEACH</Heading>
+              </CardHeader>
+              <CardBody textAlign={'center'}>
+                <Text>
+                  Premier transportation services for exploring Algarve's
+                  beautiful beaches. Trust our experienced drivers for a
+                  memorable and convenient beach getaway.
+                </Text>
+              </CardBody>
+              <Center>
+                <CardFooter>
+                  <Button bg="#ECB939">View here</Button>
+                </CardFooter>
+              </Center>
+            </Card>
+          </SimpleGrid>
+        </Card>
       </Center>
     </>
   );
