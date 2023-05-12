fdb86359f4a3dccf5c1ed28f6fc0410e159a6bfa
Felipe Justus Matos <70666131+FelipeJustusMatos@users.noreply.github.com>
2023-05-12 10:33:19 -0300

    FixedUpbarnavigation

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,11 +6,16 @@ import Formrequest from '../components/Formrequest';
 import Navbar from '../components/Navbar';
 import Services from '../components/Services';
 import Upbar from '../components/Upbar';
+import { Flex } from '@chakra-ui/react';
+import './index.css';
 export default function Home() {
   return (
     <>
-      <Upbar />
-      <Navbar />
+      <Flex className="Upbar" flexDirection={'column'}>
+        <Upbar />
+        <Navbar />
+      </Flex>
+
       <Firstscreen />
       <Cardbook />
       <Formrequest />
