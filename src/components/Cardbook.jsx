3dfcce434ccf24ec78a4d0bcee8fb1c32e44ad65
Felipe Justus Matos <70666131+FelipeJustusMatos@users.noreply.github.com>
2023-05-15 17:38:25 -0300

    update

--- src/components/Cardbook.jsx
+++ src/components/Cardbook.jsx
@@ -1,8 +1,10 @@
 import { Button, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Cardbook() {
+  const navigate = useNavigate(); // Adicione esta linha para atribuir o hook useNavigate a uma variável
+
   return (
     <Flex bg={'black'} h={100} justifyContent="center" alignItems={'center'}>
       <Text as="b" color="white" pr={5}>
@@ -13,7 +15,7 @@ export default function Cardbook() {
         bg="#ECB939"
         borderRadius={'md'}
         onClick={() => {
-          Navigate('/Booking');
+          navigate('/Booking'); // Corrigido para usar a variável navigate
         }}
       >
         Click here
