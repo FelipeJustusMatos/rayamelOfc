ffd695ac2323ac2271c03f22c7889020a9e214ae
Felipe Justus Matos <70666131+FelipeJustusMatos@users.noreply.github.com>
2023-05-15 15:01:21 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react';
+// import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import {
