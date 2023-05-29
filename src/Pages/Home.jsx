f8c8e75590e68277a33d2c0678c70936ebaf5f4d
Felipe Justus Matos <70666131+FelipeJustusMatos@users.noreply.github.com>
2023-05-29 10:37:19 -0300

    Updateform

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 import Cardbook from '../components/Cardbook';
 import Firstscreen from '../components/Firstscreen';
 import Footer from '../components/Footer';
-import Formrequest from '../components/Formrequest';
 import Navbar from '../components/Navbar';
+import { Example } from '../components/ProgressBar';
 import Services from '../components/Services';
 import Upbar from '../components/Upbar';
-import { Example } from '../components/ProgressBar';
 import './index.css';
 export default function Home() {
   return (
